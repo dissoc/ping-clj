879a7cad5e7b358d0ef5c158d17521f14dbf6e99
Justin Bishop <mail@dissoc.me>
2024-11-22 00:25:07 -0600

    initial commit

---
+++ src/ping_clj/core.clj
@@ -0,0 +1,38 @@
+(ns ping-clj.core
+  (:import
+   (com.zaxxer.ping IcmpPinger PingTarget)
+   (java.net InetAddress)))
+
+;; NOTE: if the follow exception occurs:
+;; Unable to create IPv4 socket.  If this is Linux, you might need to
+;; set sysctl net.ipv4.ping_group_range
+;; i.g. sudo sysctl net.ipv4.ping_group_range='10001 10001'
+
+(defn response-handler [& {:keys [on-response on-timeout]}]
+  (reify com.zaxxer.ping.PingResponseHandler
+    (onResponse [this target time-sec byte-count ping-seq]
+      (let [host (-> target
+                     .getInetAddress
+                     .getHostAddress)]
+        (if (nil? on-response)
+          (println (str byte-count " bytes"
+                        " from " host
+                        " icmp_seq=" ping-seq
+                        " time=" time-sec
+                        "\n"))
+          (on-response {:time      time-sec
+                        :host      host
+                        :byte-seq  ping-seq
+                        :num-bytes byte-count}))))
+    (onTimeout [this target]
+      (if (nil? on-timeout)
+        (println "timeout: " target)
+        (on-timeout target)))))
+
+(defn ping [host]
+  (let [inet-obj (InetAddress/getByName host)
+        pinger   (new IcmpPinger (response-handler))
+        target   (new PingTarget inet-obj)]
+    (future (try (.runSelector pinger)
+                 (catch Exception e (println e))))
+    (.ping pinger target)))
